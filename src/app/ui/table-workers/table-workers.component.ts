2a7c2d651ed3f9767347870b733c52505f81a9fc
Valera098 <66243570+Valera098@users.noreply.github.com>
2021-03-27 11:32:43 +0300

    Загрузил прошлую лабу

---
+++ src/app/ui/table-workers/table-workers.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {MyWorker, MyWorkerType} from 'src/app/shared/worker.model';
+
+@Component({
+  selector: 'app-table-workers',
+  templateUrl: './table-workers.component.html',
+  styleUrls: ['./table-workers.component.css'],
+})
+export class TableWorkersComponent implements OnInit {
+
+  myWorkerType = MyWorkerType;
+
+  @Input() title: string;
+  @Input() workers: MyWorker[] = [];
+
+  @Output() deleteWorker = new EventEmitter<number>();
+  @Output() editWorker = new EventEmitter<object>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDeleteWorker(id: number) {
+    this.deleteWorker.emit(id);
+  }
+  onEditWorker(id:number) {
+    console.log(id);
+    const form = Array.from(document.getElementsByName(id.toString()))
+    const [name, surname, type] = form;
+    this.editWorker.emit({id, name:name['value'], surname:surname['value'], type:Number(type['value'])})
+  }
+
+}
